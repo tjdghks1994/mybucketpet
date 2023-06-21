0978c9f5a25f1902ec105e0e434e2f1ff1ffbd0a
SungHwan <tjdghks130907@gmail.com>
2023-06-21 22:28:04 +0900

    Initial commit - 초기 셋팅 (프로젝트 생성)

---
+++ src/main/java/com/mybucketpet/MybucketpetApplication.java
@@ -0,0 +1,17 @@
+package com.mybucketpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Controller와 관련된 영역만 scan 하도록 설정
+ * Service와 Repository 관련 빈들은 별도의 Config 파일을 통해 등록 후 Import 하도록 설정
+ */
+@SpringBootApplication(scanBasePackages = "com.mybucketpet.controller")
+public class MybucketpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybucketpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybucketpet/MybucketpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybucketpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybucketpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
