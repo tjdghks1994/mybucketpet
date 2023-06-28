e33d554b40cdb5df450b861ed1cb244219966625
SungHwan <tjdghks130907@gmail.com>
2023-06-28 21:49:35 +0900

    이메일 인증 로직 추가

--- src/main/java/com/mybucketpet/controller/login/LoginController.java
+++ src/main/java/com/mybucketpet/controller/login/LoginController.java
@@ -1,14 +1,18 @@
 package com.mybucketpet.controller.login;
 
+import com.mybucketpet.service.login.EmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 @RequestMapping("/login")
+@RequiredArgsConstructor
 public class LoginController {
+
+    private final EmailService emailService;
 
     @GetMapping
     public String loginForm() {
@@ -19,5 +23,17 @@ public class LoginController {
     public String joinForm() {
         log.info("=== joinForm ===");
         return "login/joinMemberForm";
+    }
+
+    @ResponseBody
+    @PostMapping("/join/mailAuth")
+    public String mailAuthCheck(@RequestBody String email) {
+        log.info("mailAuthCheck Start");
+        log.info("email = {}", email);
+        // 인증번호 생성 후 메일 전송 - 반환값은 생성한 인증번호
+        String authCode = emailService.sendAuthCode(email);
+        log.info("LoginController authCode = {}", authCode);
+
+        return authCode;
     }
 }
