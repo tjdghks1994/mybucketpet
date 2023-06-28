d785a1ce8b68e93b763dafafde648bcedf76d9e5
SungHwan <tjdghks130907@gmail.com>
2023-06-29 00:01:46 +0900

    비밀번호 암호화 처리를 위한 설정파일을 사용하도록 수정 및 스프링 시큐리티 사용 시 자동으로 등록되는 시큐리티 관련 설정파일들을 제외하도록 설정 (exclude)

--- src/main/java/com/mybucketpet/MybucketpetApplication.java
+++ src/main/java/com/mybucketpet/MybucketpetApplication.java
@@ -3,17 +3,20 @@ package com.mybucketpet;
 import com.mybucketpet.config.AppConfig;
 import com.mybucketpet.config.JasyptConfig;
 import com.mybucketpet.config.MailConfig;
+import com.mybucketpet.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 /**
  * Controller와 관련된 영역만 scan 하도록 설정
- * Service와 Repository 관련 빈들은 별도의 Config 파일을 통해 등록 후 Import 하도록 설정
+ * Service와 Repository 관련 빈들 및 그 외의 스프링 빈으로 등록하기 위한 것은 별도의 Config 파일을 통해 등록 후 Import 하도록 설정
+ * Spring Security의 PasswordEncoder만 활용하기 위해서 자동으로 설정되는 Security 설정들 제외하도록 exclude 설정
  */
 
-@Import({JasyptConfig.class, MailConfig.class, AppConfig.class })
-@SpringBootApplication(scanBasePackages = "com.mybucketpet.controller")
+@Import({JasyptConfig.class, MailConfig.class, AppConfig.class, SecurityConfig.class})
+@SpringBootApplication(scanBasePackages = "com.mybucketpet.controller", exclude = SecurityAutoConfiguration.class)
 public class MybucketpetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MybucketpetApplication.class, args);
