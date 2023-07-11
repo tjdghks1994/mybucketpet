4882a4a7758631a8e8ac6aaee06935c8a0518fff
SungHwan <tjdghks130907@gmail.com>
2023-07-11 22:52:17 +0900

    썸네일 테이블 설계 후 썸네일 클래스 작성

---
+++ src/main/java/com/mybucketpet/domain/bucket/Thumbnail.java
@@ -0,0 +1,22 @@
+package com.mybucketpet.domain.bucket;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Thumbnail {
+    private Long thumbnailId;   // 썸네일 ID
+    private String thumbnailFilename;   // 썸네일 이미지 원본파일명
+    private String thumbnailSavename;   // 썸네일 저장파일명
+
+    public Thumbnail() { }
+
+    public Thumbnail(Long thumbnailId, String thumbnailFilename, String thumbnailSavename) {
+        this.thumbnailId = thumbnailId;
+        this.thumbnailFilename = thumbnailFilename;
+        this.thumbnailSavename = thumbnailSavename;
+    }
+}
