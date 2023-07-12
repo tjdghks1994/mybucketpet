7a5c9f847e48a11db23e4027d7aa9dfce85a3707
SungHwan <tjdghks130907@gmail.com>
2023-07-12 23:57:41 +0900

    버킷, 썸네일, 태그 등록 및 조회 개발 태그명 조회 개발

---
+++ src/main/java/com/mybucketpet/repository/bucket/BucketRepository.java
@@ -0,0 +1,18 @@
+package com.mybucketpet.repository.bucket;
+
+import com.mybucketpet.domain.bucket.Bucket;
+import com.mybucketpet.domain.bucket.Tag;
+import com.mybucketpet.domain.bucket.Thumbnail;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BucketRepository {
+    Bucket saveBucket(Bucket bucket);
+    Thumbnail saveThumbnail(Thumbnail thumbnail, Long bucketId);
+    List<Tag> saveTag(List<Tag> tagList, Long bucketId);
+    Optional<Bucket> findBucketById(Long bucketId);
+    Optional<Thumbnail> findThumbnailByBucketId(Bucket bucket);
+    List<Tag> findTagByBucketId(Bucket bucket);
+    Tag findTagNameById(Tag tag);
+}
---
+++ src/main/java/com/mybucketpet/repository/bucket/MybatisBucketRepository.java
@@ -0,0 +1,61 @@
+package com.mybucketpet.repository.bucket;
+
+import com.mybucketpet.domain.bucket.Bucket;
+import com.mybucketpet.domain.bucket.Tag;
+import com.mybucketpet.domain.bucket.Thumbnail;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+public class MybatisBucketRepository implements BucketRepository {
+
+    private final BucketMapper bucketMapper;
+    @Autowired
+    public MybatisBucketRepository(BucketMapper bucketMapper) {
+        this.bucketMapper = bucketMapper;
+    }
+
+    @Override
+    public Bucket saveBucket(Bucket bucket) {
+        bucketMapper.saveBucket(bucket);
+        return bucket;
+    }
+
+    @Override
+    public Thumbnail saveThumbnail(Thumbnail thumbnail, Long bucketId) {
+        bucketMapper.saveThumbnail(thumbnail, bucketId);
+        return thumbnail;
+    }
+
+    @Override
+    public List<Tag> saveTag(List<Tag> tagList, Long bucketId) {
+        bucketMapper.saveTag(tagList, bucketId);
+        return tagList;
+    }
+
+    @Override
+    public Optional<Bucket> findBucketById(Long bucketId) {
+        return bucketMapper.findBucketById(bucketId);
+    }
+
+    @Override
+    public Optional<Thumbnail> findThumbnailByBucketId(Bucket bucket) {
+        return bucketMapper.findThumbnailByBucketId(bucket);
+    }
+
+    @Override
+    public List<Tag> findTagByBucketId(Bucket bucket) {
+        return bucketMapper.findTagByBucketId(bucket);
+    }
+
+    @Override
+    public Tag findTagNameById(Tag tag) {
+        return bucketMapper.findTagNameById(tag);
+    }
+}
