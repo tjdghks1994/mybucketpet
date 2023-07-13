55cf0c1fe52f1d9f791ddc8095add7fe43a68352
SungHwan <tjdghks130907@gmail.com>
2023-07-14 00:13:58 +0900

    버킷 등록 기능 개발 진행 중

--- src/main/resources/static/js/admin/adminBucketAdd.js
+++ src/main/resources/static/js/admin/adminBucketAdd.js
@@ -1,3 +1,168 @@
+// 태그 select box 선택한 요소를 담고 있는 배열
+let checkTagNameList = new Array();
+// 태그 select box 선택한 요소의 값을 담고 있는 배열
+let checkTagValueList = new Array();
+// 공개 여부 값
+let openYnValue = "";
+// 추천 여부 값
+let recommendYnValue = "";
+
+/**
+ * 버킷 등록
+ */
+function addBucket() {
+    let bucketTitle = $('#bucketName').val();
+    let bucketContents = $('#summernote').summernote('code');   // 썸머노트 editor 값 가져오기
+    let openYn = openYnValue;
+    let recommendYn = recommendYnValue;
+    let tagList = new Array();
+    $.each(checkTagValueList, function (idx, value) {
+        let tag = new Object();
+        tag.tagId = value;
+
+        tagList.push(tag);
+    });
+    // 썸네일 이미지 파일
+    let thumbnailFile = $('#bucketImageFile')[0].files[0];
+    // 버킷 저장할 전송 정보
+    let bucketAdd = {
+      bucketTitle : bucketTitle,
+      bucketContents : bucketContents,
+      openYn : openYn,
+      recommendYn : recommendYn,
+      tagList : tagList
+    };
+    // file은 multipart/form-data, 그 외의 데이터는 application/json 형식으로 따로 보내기 위해 Blob을 사용
+    let formData = new FormData();
+    formData.append('thumbnailImageFile', thumbnailFile);
+    formData.append('bucketAdd', new Blob([JSON.stringify(bucketAdd)], {type: "application/json"}));
+
+    $.ajax({
+        url: contextPath+"/admin/bucket/add",
+        method: "post",
+        contentType: false,
+        processData: false,
+        data : formData,
+        cache : false,
+        success: function (result){
+            console.log(result);
+        },
+        fail: function (error) {
+            alert('버킷 등록에 실패하였습니다. 관리자에게 문의하세요.');
+        }
+    });
+}
+
+/**
+ * 썸네일 파일 선택 후 x 버튼 클릭 이벤트
+ */
+function initFileInput() {
+    let fileDelYn = confirm("정말 해당 파일 첨부를 취소하시겠습니까?");
+    if (fileDelYn) {
+        // input file 태그 값 초기화
+        $('#bucketImageFile').val('');
+        $('#file-append-name').text('');
+        $('.file-remove-btn').remove();
+    }
+}
+
+/**
+ * 공개 여부, 추천 여부 버튼 클릭 이벤트
+ */
+function activeSelectButton(buttonObj) {
+    let selectValue = $(buttonObj).val();
+
+    if ($(buttonObj).hasClass('recommendYn')) {
+        recommendYnValue = selectValue;
+        // 모든 버튼 활성화 클래스 삭제 처리
+        $.each($('button.recommendYn'), function (idx, btn) {
+            $(btn).removeClass('active');
+        });
+        // 선택한 버튼만 활성화 클래스 추가
+        $(buttonObj).addClass('active');
+    } else if ($(buttonObj).hasClass('openYn')) {
+        openYnValue = selectValue;
+        // 모든 버튼 활성화 클래스 삭제 처리
+        $.each($('button.openYn'), function (idx, btn) {
+            $(btn).removeClass('active');
+        });
+        // 선택한 버튼만 활성화 클래스 추가
+        $(buttonObj).addClass('active');
+    }
+}
+
+/**
+ * 태그 select box list 클릭 이벤트
+ */
+function checkTagList(checkObj) {
+    // 체크 한 경우
+    if (checkObj.checked) {
+        // 배열에 저장
+        checkTagNameList.push(checkObj.id);
+        checkTagValueList.push(checkObj.value);
+    } else { // 체크 해제한 경우
+        for (let i = 0; i < checkTagNameList.length; i++) {
+            if (checkTagNameList[i] === checkObj.id) {
+                // 배열에서 제거
+                checkTagNameList.splice(i, 1);
+                checkTagValueList.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    // 선택된 태그 명 출력
+    $('#bucketTag-box p').text(checkTagNameList.join());
+    $('#select-tag-content').text(checkTagNameList.join(", "));
+}
+
+/**
+ * 태그 select box 이외의 영역 클릭 시
+ * select box list 숨김 처리
+ */
+function hideTagList() {
+    // select box 영역 제외하고 클릭했을 시 숨김처리.
+    $('body').on('click', function(e){
+        if ($(e.target).closest("#bucketTag").length == 0) {
+            $('#bucketTag-listUL').hide();
+        }
+    });
+
+}
+/**
+ * 태그 select box 클릭 이벤트
+ * 목록이 보이는 경우 숨김 처리
+ * 목록이 보이지 않는 경우 보이도록 처리
+ */
+function showTagList() {
+    $('#bucketTag-box').on('click', function () {
+        if ($('#bucketTag-listUL').css('display') != 'none') {
+            $('#bucketTag-listUL').hide();
+        } else {
+            $('#bucketTag-listUL').show();
+        }
+    });
+}
+
+/**
+ * 태그 select box 렌더링
+ */
+function initTagRendering() {
+    $.ajax({
+        url : contextPath + "/admin/bucket/tag",
+        method : "get",
+        contentType: "application/json",
+        success: function (data) {
+            $.each(data, function (idx, tagItem) {
+                $('#bucketTag ul').append('<li><input type="checkbox" onclick="checkTagList(this);" value=' + tagItem.tagId +
+                    ' id='+tagItem.tagName+'> <label for="'+ tagItem.tagName+'">'+ tagItem.tagName+'</label></li>');
+            });
+        },
+        fail: function (error) {
+            alert('태그 목록을 가져오는데 오류가 발생했습니다. 관리자에게 문의하세요');
+        }
+    });
+}
+
 /**
  * 에디터 초기화
  */
@@ -29,6 +194,10 @@ function initEditor() {
 function uploadImageFile(file) {
     let uploadImg = file.files[0];
     $('#file-append-name').text(uploadImg['name']);
+    if ($('.file-remove-btn').length == 0) {
+        $('.file-append-name-wrapper').append('<img class="file-remove-btn" onclick="initFileInput();"' +
+            ' src="'+ contextPath +'/img/ic_close.svg"/>' );
+    }
 }
 
 /**
@@ -70,4 +239,7 @@ $(function () {
     openMenu();
     changeMenuTitle();
     initEditor();
+    initTagRendering();
+    showTagList();
+    hideTagList();
 });
