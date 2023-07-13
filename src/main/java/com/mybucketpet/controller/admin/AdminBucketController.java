93a5980aceda61768a05c20088681102b9ee4f4f
SungHwan <tjdghks130907@gmail.com>
2023-07-14 00:13:31 +0900

    버킷 등록 및 모든 태그 목록 조회 추가

--- src/main/java/com/mybucketpet/controller/admin/AdminBucketController.java
+++ src/main/java/com/mybucketpet/controller/admin/AdminBucketController.java
@@ -1,15 +1,37 @@
 package com.mybucketpet.controller.admin;
 
+import com.mybucketpet.domain.bucket.Tag;
+import com.mybucketpet.service.bucket.BucketService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/admin/bucket")
 public class AdminBucketController {
-
+    private final BucketService bucketService;
+    @Autowired
+    public AdminBucketController(BucketService bucketService) {
+        this.bucketService = bucketService;
+    }
+    /**
+     * HTTP URI 설계
+     * 버킷 관리 페이지 : /admin/bucket    GET
+     * 버킷 등록 페이지 : /admin/bucket/add    GET
+     * === HTTP API 사용 ===
+     * 버킷 등록 : /admin/bucket/add    POST
+     * 버킷 단일 조회 : /admin/bucket/{bucketId}  GET
+     * 버킷 다중 조회(목록) : /admin/bucket/list    GET
+     * 태그 조회(목록) : /admin/bucket/tag    GET
+     */
     @GetMapping
     public String bucketManageForm() {
         return "admin/bucket/bucket_manage";
@@ -18,5 +40,23 @@ public class AdminBucketController {
     @GetMapping("/add")
     public String bucketManageAddForm() {
         return "admin/bucket/bucket_manage_addForm";
+    }
+
+    @PostMapping(value = "/add")
+    public ResponseEntity<String> bucketAdd(@RequestPart("bucketAdd") BucketAdd bucketAdd,
+                                            @RequestPart("thumbnailImageFile") MultipartFile thumbnailImgFile) {
+        log.info("bucketAdd = {}", bucketAdd);
+        log.info("thumbnailImgFile = {}", thumbnailImgFile.getOriginalFilename());
+
+        return new ResponseEntity<>("addBucketOK", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/tag")
+    @ResponseBody
+    public List<Tag> getTagList() {
+        List<Tag> allTag = bucketService.findAllTag();
+        log.debug("allTag = {}", allTag);
+
+        return allTag;
     }
 }
