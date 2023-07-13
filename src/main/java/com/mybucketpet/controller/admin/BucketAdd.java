5aa0962467e8c4cebeb2a45c5f21ef7bc2b9ba90
SungHwan <tjdghks130907@gmail.com>
2023-07-14 00:12:58 +0900

    버킷 등록시 사용하는 클래스에서 필요 없는 멤버변수 삭제 처리 및 생성자 수정

--- src/main/java/com/mybucketpet/controller/admin/BucketAdd.java
+++ src/main/java/com/mybucketpet/controller/admin/BucketAdd.java
@@ -3,29 +3,26 @@ package com.mybucketpet.controller.admin;
 import com.mybucketpet.domain.bucket.Tag;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class BucketAdd {
     private String bucketTitle; // 버킷 제목
     private String bucketContents;  // 버킷 내용
     private String openYn;  // 공개 여부
     private String recommendYn; // 추천 여부
-    private String thumbnailFilename;   // 썸네일 이미지 원본파일명
-    private String thumbnailSavename;   // 썸네일 저장파일명
     private List<Tag> tagList; // 태그 ID들
 
     public BucketAdd() { }
-    public BucketAdd(String bucketTitle, String bucketContents, String openYn, String recommendYn,
-                     String thumbnailFilename, String thumbnailSavename, List<Tag> tagList) {
+    public BucketAdd(String bucketTitle, String bucketContents, String openYn, String recommendYn, List<Tag> tagList) {
         this.bucketTitle = bucketTitle;
         this.bucketContents = bucketContents;
         this.openYn = openYn;
         this.recommendYn = recommendYn;
-        this.thumbnailFilename = thumbnailFilename;
-        this.thumbnailSavename = thumbnailSavename;
         this.tagList = tagList;
     }
 }
