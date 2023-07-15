aa612bc398923a71ba7cba215ddd001e60971aa5
SungHwan <tjdghks130907@gmail.com>
2023-07-15 21:17:16 +0900

    버킷 목록 조회 조건 정보를 담고 있는 클래스 추가

---
+++ src/main/java/com/mybucketpet/controller/admin/BucketSearch.java
@@ -0,0 +1,29 @@
+package com.mybucketpet.controller.admin;
+
+import com.mybucketpet.domain.bucket.Tag;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+public class BucketSearch { // 버킷 목록 조회 클래스
+    private String keywordType;
+    private String keywordText;
+    private String openYn;
+    private String recommendYn;
+    private List<Tag> tagList;
+
+    public BucketSearch() { }
+
+    public BucketSearch(String keywordType, String keywordText, String openYn, String recommendYn, List<Tag> tagList) {
+        this.keywordType = keywordType;
+        this.keywordText = keywordText;
+        this.openYn = openYn;
+        this.recommendYn = recommendYn;
+        this.tagList = tagList;
+    }
+}
