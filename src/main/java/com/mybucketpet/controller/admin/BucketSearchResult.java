06967ff39932efdd5c96c4a507f5d84b660e0521
SungHwan <tjdghks130907@gmail.com>
2023-07-15 21:17:36 +0900

    조회한 버킷의 정보를 담고 있는 클래스 추가

---
+++ src/main/java/com/mybucketpet/controller/admin/BucketSearchResult.java
@@ -0,0 +1,26 @@
+package com.mybucketpet.controller.admin;
+
+import com.mybucketpet.domain.bucket.Tag;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+@ToString
+public class BucketSearchResult {   // 조회한 버킷 정보를 담고 있는 클래스
+    private Long rnum;
+    private String bucketId;
+    private String bucketTitle;
+    private Long certiCnt;
+    private Long scrapCnt;
+    private Long lookupCnt;
+    private LocalDate modifyDate;
+    private String openYn;
+    private String recommendYn;
+
+    public BucketSearchResult() { }
+}
