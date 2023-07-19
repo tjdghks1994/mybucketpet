4a1b914b57e37a96bc47945363ef2fd7034ee308
SungHwan <tjdghks130907@gmail.com>
2023-07-19 10:28:16 +0900

    태그 객체 비교시 동일한 id를 가진 경우 동일 객체로 판단하기 위해 equals, hashCode메서드 Overriding

--- src/main/java/com/mybucketpet/domain/bucket/Tag.java
+++ src/main/java/com/mybucketpet/domain/bucket/Tag.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -15,5 +17,20 @@ public class Tag {
 
     public Tag(Long tagId) {
         this.tagId = tagId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagId);
+    }
+
+    @Override
+    public boolean equals(Object obj) { // 태그 ID가 같으면 동일한 태그 객체
+        if (!(obj instanceof Tag)) {
+            throw new IllegalArgumentException("Object is not a Tag type");
+        }
+        Tag tag = (Tag) obj;
+
+        return tag.getTagId() == this.tagId;
     }
 }
