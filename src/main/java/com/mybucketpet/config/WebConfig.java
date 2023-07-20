6be86029bf07a73a8c938095fb387494cdc75d53
SungHwan <tjdghks130907@gmail.com>
2023-07-20 23:09:51 +0900

    관리자가 접근하기 위한 URL 패턴으로 관리자 체크 인터셉터 등록

---
+++ src/main/java/com/mybucketpet/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.mybucketpet.config;
+
+import com.mybucketpet.interceptor.AdminCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/admin/**");
+    }
+}
