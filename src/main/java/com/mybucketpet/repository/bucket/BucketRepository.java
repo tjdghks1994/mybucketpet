17287f8bea279b79e7140a4bff25bbfc6d7037d1
SungHwan <tjdghks130907@gmail.com>
2023-10-17 22:33:10 +0900

    [refactor] 도메인(엔티티) 클래스 의존성 제거
    - Service <-> Repository 영역에서 도메인 클래스에 대한 의존성 제거함 (Repository <-> Mapper 영역에서만 도메인 클래스에 대해 의존)
    - 매개변수로 도메인 클래스를 사용하던 부분들에 대해 DTO 클래스를 사용하도록 변경함

--- src/main/java/com/mybucketpet/repository/bucket/BucketRepository.java
+++ src/main/java/com/mybucketpet/repository/bucket/BucketRepository.java
@@ -1,31 +1,30 @@
 package com.mybucketpet.repository.bucket;
 
-import com.mybucketpet.controller.admin.dto.BucketSearchResult;
+import com.mybucketpet.controller.admin.dto.*;
 import com.mybucketpet.controller.paging.PageMakeVO;
 import com.mybucketpet.domain.bucket.Bucket;
 import com.mybucketpet.domain.bucket.BucketThumbnail;
 import com.mybucketpet.domain.bucket.Tag;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BucketRepository {
-    Bucket saveBucket(Bucket bucket);
-    BucketThumbnail saveThumbnail(BucketThumbnail thumbnail, Long bucketId);
-    List<Tag> saveTag(List<Tag> tagList, Long bucketId);
-    Optional<Bucket> findBucketById(Long bucketId);
-    Optional<BucketThumbnail> findThumbnailByBucketId(Long bucketId);
-    List<Tag> findTagByBucketId(Long bucketId);
-    Tag findTagNameById(Tag tag);
-    List<Tag> findAllTag();
+    Long saveBucket(BucketAdd bucketAdd);
+    Long saveThumbnail(String fileOriginalName, String fileSaveName, Long bucketId);
+    List<String> saveTag(Long bucketId, List<String> tagIdList);
+    BucketInfo findBucketById(Long bucketId);
+    ThumbnailInfo findThumbnailByBucketId(Long bucketId);
+    List<TagInfo> findTagByBucketId(Long bucketId);
+    List<TagInfo> findAllTag();
     int getTotalBucketCount();
-    List<BucketSearchResult> findAllBucket(String keywordType, String keywordText,
-                                           Bucket bucketSearch, List<Tag> tagList, PageMakeVO pageMakeVO);
+    List<BucketSearchResult> findAllBucket(BucketSearch bucketSearch, PageMakeVO pageMakeVO);
     void deleteBucket(Long bucketId);
     void deleteThumbnail(Long bucketId);
     void deleteTag(Long bucketId);
     void updateBucketRecommend(Long bucketId, String recommendYn);
-    void updateBucket(Long bucketId, Bucket bucketUpdate);
-    void updateThumbnail(Long bucketId, BucketThumbnail thumbnail);
-    void deleteTagList(List<Tag> deleteTagList, Long bucketId);
+    void updateBucket(Long bucketId, BucketUpdate bucketUpdate);
+    void updateThumbnail(Long bucketId, String fileOriginalName, String fileSaveName);
+    void deleteTagList(Long bucketId, List<String> deleteTagIdList);
 }
