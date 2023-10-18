83ff66298ee5a4294a9a6ff4bf98a8edc8fce362
SungHwan <tjdghks130907@gmail.com>
2023-10-18 23:52:37 +0900

    [refactor] Service 소스 코드 수정으로 인한 기능테스트 진행
    - 기존 소스코드로 동작하던 테스트 코드에 대하여 변경된 소스 코드로 기능이 제대로 동작하는지 테스트 코드 새로작성 후 테스트 진행함
    - Repository 위임 역할만 하는 메서드에 대해서는 테스트 진행 안함

--- src/test/java/com/mybucketpet/service/bucket/BucketServiceImplTest.java
+++ src/test/java/com/mybucketpet/service/bucket/BucketServiceImplTest.java
@@ -1,7 +1,8 @@
 package com.mybucketpet.service.bucket;
 
-import com.mybucketpet.controller.admin.BucketAdd;
-import com.mybucketpet.controller.admin.BucketInfo;
+import com.mybucketpet.controller.admin.dto.BucketAdd;
+import com.mybucketpet.controller.admin.dto.BucketResponse;
+import com.mybucketpet.controller.admin.dto.BucketUpdate;
 import com.mybucketpet.domain.bucket.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -9,12 +10,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -28,16 +32,65 @@ class BucketServiceImplTest {
     }
     @Test
     @DisplayName("버킷 등록 및 조회 테스트")
-    void saveAndFindTest() {
+    void saveAndFindTest() throws IOException {
         // given
-        List<Tag> tagList = Arrays.asList(new Tag(1L), new Tag(2L), new Tag(3L));
-        BucketAdd bucketAdd = new BucketAdd("bucketTest", "bucketContents~~~", "y",
-                "y", tagList);
+        BucketAdd bucketAdd = BucketAdd.builder()
+                .bucketTitle("등록 테스트")
+                .bucketContents("버킷 등록 테스트 내용 입니다")
+                .openYn("y")
+                .recommendYn("n")
+                .tagList(Arrays.asList("1", "2", "3"))
+                .build();
+        MockMultipartFile thumbnailImageFile = getMockMultipartFile();
         // when
-//        Long saveBucketId = service.save(bucketAdd);
-//        BucketInfo bucketInfo = service.findById(saveBucketId);
-//        // then
-//        Assertions.assertThat(saveBucketId).isEqualTo(bucketInfo.getBucketId());
-//        Assertions.assertThat(bucketInfo.getTagList().get(0).getTagName()).isEqualTo("강아지");
+        Long saveBucketId = service.save(bucketAdd, thumbnailImageFile);
+        BucketResponse findBucket = service.findById(saveBucketId);
+        // then
+        Assertions.assertThat(bucketAdd.getBucketTitle()).isEqualTo(findBucket.getBucketTitle());
+        Assertions.assertThat(bucketAdd.getBucketContents()).isEqualTo(findBucket.getBucketContents());
+        Assertions.assertThat(bucketAdd.getOpenYn()).isEqualTo(findBucket.getOpenYn());
+        Assertions.assertThat(bucketAdd.getRecommendYn()).isEqualTo(findBucket.getRecommendYn());
+        Assertions.assertThat(bucketAdd.getTagList().size()).isEqualTo(findBucket.getTagList().size());
+        Assertions.assertThat(thumbnailImageFile.getOriginalFilename()).isEqualTo(findBucket.getThumbnailFilename());
+    }
+
+    @Test
+    @DisplayName("버킷 수정 테스트")
+    void updateBucket() throws IOException {
+        // given
+        Long bucketId = 22L;
+        BucketUpdate bucketUpdate = BucketUpdate
+                .builder()
+                .bucketTitle("버킷 수정합니다")
+                .bucketContents("버킷 내용 수정입니다.")
+                .openYn("n")
+                .recommendYn("y")
+                .insertTagList(Arrays.asList("1", "2"))
+                .deleteTagList(Arrays.asList("4"))
+                .build();
+        MockMultipartFile thumbnailImageFile = getMockMultipartFile();
+        // when
+        BucketResponse originalFindBucket = service.findById(bucketId);
+        service.updateBucket(bucketId, bucketUpdate, thumbnailImageFile);
+        BucketResponse updateFindBucket = service.findById(bucketId);
+        // then
+        Assertions.assertThat(bucketUpdate.getBucketTitle()).isEqualTo(updateFindBucket.getBucketTitle());
+        Assertions.assertThat(bucketUpdate.getBucketContents()).isEqualTo(updateFindBucket.getBucketContents());
+        Assertions.assertThat(bucketUpdate.getOpenYn()).isEqualTo(updateFindBucket.getOpenYn());
+        Assertions.assertThat(bucketUpdate.getRecommendYn()).isEqualTo(updateFindBucket.getRecommendYn());
+        Assertions.assertThat(originalFindBucket.getTagList().size()).isNotEqualTo(updateFindBucket.getTagList().size());
+        Assertions.assertThat(originalFindBucket.getThumbnailFilename()).isNotEqualTo(updateFindBucket.getThumbnailFilename());
+    }
+
+    /**
+     * MultiPartFile 파라미터가 필요하여 MockMultipartFile 객체 생성 메서드
+     */
+    private MockMultipartFile getMockMultipartFile() throws IOException {
+        String fileName = "test";
+        String contentType = "png";
+        String filePath = "/Users/parksunghwan/desktop/projects/mbpuploadfile/bucket/test.png";
+        FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+
+        return new MockMultipartFile(fileName, fileName + "." + contentType, contentType, fileInputStream);
     }
 }
