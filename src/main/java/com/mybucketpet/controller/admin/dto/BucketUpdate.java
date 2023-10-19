80a6fead6f1e2184d7d530544a06cfeb7a693bab
SungHwan <tjdghks130907@gmail.com>
2023-10-20 01:09:45 +0900

    [refactor] 검증 예외 기본 메시지 추가
    - 검증 예외 발생 시 기본 메시지 내용 추가함

--- src/main/java/com/mybucketpet/controller/admin/dto/BucketUpdate.java
+++ src/main/java/com/mybucketpet/controller/admin/dto/BucketUpdate.java
@@ -12,12 +12,12 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BucketUpdate {
-    @NotBlank
+    @NotBlank(message = "버킷 제목을 입력해주세요.")
     private String bucketTitle; // 버킷 제목
     private String bucketContents;  // 버킷 내용
-    @NotBlank
+    @NotBlank(message = "버킷의 공개여부를 선택해주세요.")
     private String openYn;  // 공개 여부
-    @NotBlank
+    @NotBlank(message = "버킷의 추천여부를 선택해주세요.")
     private String recommendYn; // 추천 여부
     private List<String> insertTagList; // 새로 추가된 태그 ID 목록
     private List<String> deleteTagList; // 삭제 할 태그 ID 목록
