5586646ca6f1fb0fa9feb0af3546b563c18d811d
SungHwan <tjdghks130907@gmail.com>
2023-10-20 17:49:06 +0900

    [refactor] HTTP API 설계 변경
    - URI의 리소스는 복수로 변경 처리함
    - 컨트롤러의 반환 타입을 ResponseEntity 타입으로 변경함
    - 검증예외 처리는 글로벌 ExceptionHanlder 클래스로 처리하여 관련 로직 삭제 처리함

--- src/main/java/com/mybucketpet/controller/admin/AdminApiController.java
+++ src/main/java/com/mybucketpet/controller/admin/AdminApiController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -57,16 +54,16 @@ public class AdminApiController {
     /**
      * HTTP URI 설계 - API
      * 버킷 목록 조회            /admin/buckets                          GET
-     * 버킷 등록               /admin/buckets                           POST
-     * 버킷 복수 삭제           /admin/buckets                           DELETE
+     * 버킷 등록                /admin/buckets                          POST
      * 버킷 단건 조회            /admin/buckets/{bucketId}               GET
      * 버킷 수정               /admin/buckets/{bucketId}                PATCH
-     * 버킷 단건 삭제           /admin/buckets/{bucketId}                DELETE
+     * 버킷 삭제               /admin/buckets/{bucketId}                DELETE
+     * 버킷 추천 여부 변경       /admin/buckets/{bucketId}/recommends     PATCH
      * 버킷 썸네일 이미지 조회     /admin/buckets/thumbnails/{filename}     GET
      * 버킷 태그 조회           /admin/buckets/tags                      GET
-     * 버킷 추천 여부 변경       /admin/buckets/recommends                PATCH
      */
 
+    // 버킷 목록 조회
     @GetMapping
     public ResponseEntity<List<BucketSearchResult>> bucketManageList(@RequestBody BucketSearch bucketSearch) {
         log.debug("bucketSearch = {}", bucketSearch);
@@ -81,6 +78,7 @@ public class AdminApiController {
         return ResponseEntity.ok(bucketList);
     }
 
+    // 버킷 등록
     @PostMapping
     public ResponseEntity<Void> bucketAdd(
             @Valid @RequestPart("bucketAdd") BucketAdd bucketAdd,
@@ -93,6 +91,58 @@ public class AdminApiController {
         return ResponseEntity.created(location).build();
     }
 
+    // 버킷 단건 조회
+    @GetMapping("/{bucketId}")
+    public ResponseEntity<BucketResponse> bucketDetailInfo(@PathVariable Long bucketId) {
+        BucketResponse findBucket = bucketService.findById(bucketId);
+
+        return ResponseEntity.ok(findBucket);
+    }
+
+    // 버킷 수정
+    @PatchMapping("/{bucketId}")
+    public ResponseEntity<BucketResponse> updateBucket(@PathVariable Long bucketId,
+                           @Validated @RequestPart("bucketUpdate") BucketUpdate bucketUpdate,
+                           @RequestPart(value = "thumbnailImageFile", required = false) MultipartFile multipartFile)
+            throws IOException {
+        log.debug("update bucketId = {}", bucketId);
+        log.debug("bucketUpdate = {}", bucketUpdate);
+        log.debug("thumbnailImageFile = {}", multipartFile);
+
+        // 버킷 수정
+        bucketService.updateBucket(bucketId, bucketUpdate, multipartFile);
+        BucketResponse updateBucket = bucketService.findById(bucketId);
+
+        return ResponseEntity.ok(updateBucket);
+    }
+
+    // 버킷 삭제
+    @DeleteMapping("/{bucketId}")
+    public ResponseEntity<Void> deleteBucketList(@PathVariable Long bucketId) {
+        log.debug("bucketId = {}", bucketId);
+        // 버킷 삭제
+        bucketService.deleteBucket(bucketId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // 버킷 추천 여부 변경
+    @PatchMapping("/{bucketId}/recommends")
+    @ResponseBody
+    public ResponseEntity<Void> updateBucketRecommend(@PathVariable Long bucketId,
+                                            @RequestBody String changeRecommendValue) {
+        log.debug("bucketId = {}, changeRecommendValue={}", bucketId, changeRecommendValue);
+        bucketService.updateBucketRecommend(bucketId, changeRecommendValue);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // 버킷 썸네일 이미지 조회
+    @GetMapping("/images/{filename}")
+    public Resource showImage(@PathVariable String filename) throws MalformedURLException {
+        return new UrlResource("file:" + savePath + filename);
+    }
+    // 버킷 태그 조회
     @GetMapping("/tags")
     public List<TagInfo> getTagList() {
         List<TagInfo> allTag = bucketService.findAllTag();
@@ -101,71 +151,4 @@ public class AdminApiController {
         return allTag;
     }
 
-    @DeleteMapping
-    public List<Long> deleteBucketList(@RequestBody List<Long> bucketIdList) {
-        log.debug("bucketIdList = {}", bucketIdList);
-        List<Long> failBucketList = new ArrayList<>();
-        for (Long bucketId : bucketIdList) {
-            try {
-                // 버킷 삭제
-                bucketService.deleteBucket(bucketId);
-            } catch (Exception e) {
-                failBucketList.add(bucketId);
-                log.error("버킷 ID = {}", bucketId);
-                log.error("버킷을 삭제하는데 오류가 발생하였습니다.", e);
-            }
-        }
-
-        return failBucketList;
-    }
-
-    @PatchMapping("/recommend")
-    @ResponseBody
-    public List<Long> updateBucketRecommend(@RequestBody List<BucketRecommendInfo> updateBucketRecommendInfoList) {
-        log.debug("updateBucketRecommendInfo = {}", updateBucketRecommendInfoList);
-        List<Long> failBucketList = new ArrayList<>();
-        for (BucketRecommendInfo updateBucket : updateBucketRecommendInfoList) {
-            try {
-                // 버킷 추천 값 변경
-                bucketService.updateBucketRecommend(updateBucket);
-            } catch (Exception e) {
-                Long bucketId = updateBucket.getBucketId();
-                failBucketList.add(bucketId);
-                log.error("버킷 ID = {}", bucketId);
-                log.error("버킷의 추천 값을 변경하는데 오류가 발생하였습니다.", e);
-            }
-        }
-
-        return failBucketList;
-    }
-
-    @PatchMapping("/{bucketId}")
-    public ResponseEntity<String> updateBucket(@PathVariable String bucketId,
-                                               @Validated @RequestPart("bucketUpdate") BucketUpdate bucketUpdate,
-                                               BindingResult bindingResult,
-                                               @RequestPart(value = "thumbnailImageFile", required = false) MultipartFile multipartFile)
-                                                throws IOException {
-        log.debug("update bucketId = {}", bucketId);
-        log.debug("bucketUpdate = {}", bucketUpdate);
-        log.debug("thumbnailImageFile = {}", multipartFile);
-
-        if (bindingResult.hasErrors()) {
-            log.debug("bindingResult = {}", bindingResult);
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-            String errorCode = Arrays.stream(fieldErrors.get(0).getCodes()).findFirst().get();
-            // bindingResult에서 반환되는 필드 에러가 발생한 코드 값을 이용해서 messageSource를 활용해 errors.properties에 등록된 값 가져오기
-            String validMessage = messageSource.getMessage(errorCode, null, Locale.KOREA);
-
-            return new ResponseEntity<>(validMessage, HttpStatus.BAD_REQUEST);
-        }
-
-        // 버킷 수정
-        bucketService.updateBucket(Long.parseLong(bucketId), bucketUpdate, multipartFile);
-
-        return new ResponseEntity<>("updateBucketSuccess", HttpStatus.OK);
-    }
-    @GetMapping("/images/{filename}")
-    public Resource showImage(@PathVariable String filename) throws MalformedURLException {
-        return new UrlResource("file:" + savePath + filename);
-    }
 }
