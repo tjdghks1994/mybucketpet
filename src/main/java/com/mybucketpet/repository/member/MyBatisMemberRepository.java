8cfc100271b126dd3132e11c31276c6e4056f00c
SungHwan <tjdghks130907@gmail.com>
2023-10-24 00:08:28 +0900

    [refactor] DTO 클래스를 사용하도록 변경, 새로운 메서드 추가
    - Entity 클래스를 반환하던 메서드의 반환타입을 DTO 클래스로 반환하도록 변경함
    - 회원 중복 확인을 위한 비즈니스 로직이 추가됨으로 써, 회원 중복 조회 메서드 추가함

--- src/main/java/com/mybucketpet/repository/member/MyBatisMemberRepository.java
+++ src/main/java/com/mybucketpet/repository/member/MyBatisMemberRepository.java
@@ -1,6 +1,9 @@
 package com.mybucketpet.repository.member;
 
+import com.mybucketpet.controller.member.dto.LoginForm;
+import com.mybucketpet.controller.member.dto.ResponseJoinInfo;
 import com.mybucketpet.domain.member.Member;
+import com.mybucketpet.exception.member.NotFoundMemberException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
@@ -16,14 +19,22 @@ public class MyBatisMemberRepository implements MemberRepository {
     }
 
     @Override
-    public Member save(Member member) {
+    public ResponseJoinInfo save(Member member) {
         memberMapper.save(member);
-        return member;
+        return ResponseJoinInfo.builder().member(member).build();
     }
 
     @Override
-    public Optional<Member> findById(String memberId) {
-        return memberMapper.findById(memberId);
+    public LoginForm findById(String memberId) {
+        Member findMember = memberMapper.findById(memberId)
+                .orElseThrow(() -> new NotFoundMemberException("회원을 찾을 수 없습니다."));
+
+        return LoginForm.of(findMember.getMemberId(), findMember.getMemberPw());
+    }
+
+    @Override
+    public Optional<String> findByIdDuplicate(String memberId) {
+        return memberMapper.findByIdDuplicate(memberId);
     }
 
     @Override
