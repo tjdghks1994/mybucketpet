5e1c7934fe09100a867eb616914b00d8b39bc88f
SungHwan <tjdghks130907@gmail.com>
2023-10-24 00:02:20 +0900

    [refactor] 커스텀 예외 클래스 생성
    - MemberException을 상속받은 커스텀 예외 클래스를 작성함
    - 중복된 회원일 경우 사용하기 위한 커스텀 클래스

---
+++ src/main/java/com/mybucketpet/exception/member/DuplicateMemberException.java
@@ -0,0 +1,7 @@
+package com.mybucketpet.exception.member;
+
+public class DuplicateMemberException extends MemberException {
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+}
