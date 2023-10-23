4e68a07180efb9421b6eb26a207b6be0b2df5b45
SungHwan <tjdghks130907@gmail.com>
2023-10-23 23:52:35 +0900

    [refactor] 빌더 패턴을 사용하도록 리팩토링, 필드 변경, Bean Validation 수정
    - 점층적 생성자 패턴과 자바빈즈 패턴을 사용하지 않고, 빌더 패턴을 사용하도록 변경함
    - @Setter 삭제, Lombok의 @Builder 추가, 모든 인자를 가진 생성자의 접근제어를 private로 설정함 ( Lombok의 @Builder를 사용하면 모든 인자를 가진 생성자를 자동 추가해주므로 접근제어자를 변경 처리한 것 )
    - 요청 DTO 클래스이므로 기본생성자는 필요해 @NoArgsContructor 추가 후, 접근제어를 private로 설정함
    - 기존 필드에서 필요없는 필드 삭제 후 변경 함
    - 필드가 일부 변경됨으로써 Bean Validation 수정하고, 기본 메시지 내용을 추가함

--- src/main/java/com/mybucketpet/controller/member/dto/JoinForm.java
+++ src/main/java/com/mybucketpet/controller/member/dto/JoinForm.java
@@ -1,62 +1,38 @@
 package com.mybucketpet.controller.member.dto;
 
 import com.mybucketpet.domain.member.JoinType;
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
-@Setter
+@Builder
 @ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class JoinForm {
-    @NotBlank
-    private String joinEmailPrefix;   // 회원 아이디(이메일) 접두사
-    @NotBlank
-    private String joinEmailSuffix;   // 회원 아이디(이메일) 접미사 (셀렉박스)
-    private String joinEmailSuffixSelf; // 회원 아이디(이메일) 접미사 (직접입력)
-    @NotBlank
+    @Email(message = "이메일 형식이 올바르지 않습니다.")  // 이메일 validation - 빈 문자열("")과 null 값을 허용됨
+    @NotEmpty(message = "이메일을 입력해 주세요.")
+    private String joinEmail;   // 회원 아이디(이메일)
+    @NotBlank(message = "인증이 완료되지 않았습니다.")
     private String authCode;        // 메일 인증 코드
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()-_+|=]{8,20}$")
-    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()-_+|=]{8,20}$",
+            message = "비밀번호 형식이 올바르지 않습니다.")
+    @NotBlank(message = "비밀번호를 입력해 주세요.")
     private String joinPassword;      // 회원 비밀번호
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()-_+|=]{8,20}$")
-    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()-_+|=]{8,20}$",
+            message = "비밀번호 형식이 올바르지 않습니다.")
+    @NotBlank(message = "비밀번호 확인란을 입력해주세요.")
     private String joinPasswordCheck; // 회원 비밀번호 확인
-    @NotBlank
-    @Size(min = 2, max = 15)
+    @NotBlank(message = "닉네임을 입력해 주세요.")
+    @Size(min = 2, max = 15, message = "닉네임 형식이 올바르지 않습니다.")
     private String joinNickname;      // 회원 닉네임
-    @AssertTrue
+    @AssertTrue(message = "이용약관을 동의해주세요.")
     private boolean termsUse;         // 이용약관 동의여부
-    @AssertTrue
+    @AssertTrue(message = "개인정보수집에 동의해주세요.")
     private boolean privacyUse;        // 개인정보수집 동의여부
     private boolean marketingUse;      // 마케팅활용 동의여부
-    private JoinType joinPath;          // 가입경로
+    private JoinType joinType;          // 가입경로
 
-    public JoinForm() { }
-
-    public JoinForm(JoinType joinPath) {
-        this.joinPath = joinPath;
-    }
-
-    public JoinForm(String joinEmailPrefix, String joinEmailSuffix, String joinEmailSuffixSelf, String authCode,
-                    String joinPassword, String joinPasswordCheck, String joinNickname, boolean termsUse,
-                    boolean privacyUse, boolean marketingUse, JoinType joinPath) {
-        this.joinEmailPrefix = joinEmailPrefix;
-        this.joinEmailSuffix = joinEmailSuffix;
-        this.joinEmailSuffixSelf = joinEmailSuffixSelf;
-        this.authCode = authCode;
-        this.joinPassword = joinPassword;
-        this.joinPasswordCheck = joinPasswordCheck;
-        this.joinNickname = joinNickname;
-        this.termsUse = termsUse;
-        this.privacyUse = privacyUse;
-        this.marketingUse = marketingUse;
-        this.joinPath = joinPath;
-    }
 }
