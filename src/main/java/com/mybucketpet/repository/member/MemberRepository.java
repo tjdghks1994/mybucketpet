ef9c3e4e6abff14068fcd830fc79c83dbc28bc5f
SungHwan <tjdghks130907@gmail.com>
2023-10-24 00:07:35 +0900

    [refactor] DTO 클래스를 사용하도록 변경, 새로운 메서드 추가
    - Entity 클래스를 반환하던 메서드의 반환타입을 DTO 클래스로 반환하도록 변경함
    - 회원 중복 확인을 위한 비즈니스 로직이 추가됨으로 써, 회원 중복 조회 메서드 추가함

--- src/main/java/com/mybucketpet/repository/member/MemberRepository.java
+++ src/main/java/com/mybucketpet/repository/member/MemberRepository.java
@@ -1,5 +1,8 @@
 package com.mybucketpet.repository.member;
 
+import com.mybucketpet.controller.member.dto.JoinForm;
+import com.mybucketpet.controller.member.dto.LoginForm;
+import com.mybucketpet.controller.member.dto.ResponseJoinInfo;
 import com.mybucketpet.domain.member.Member;
 
 import java.util.Optional;
@@ -7,12 +10,13 @@ import java.util.Optional;
 public interface MemberRepository {
 
     // 회원 가입
-    Member save(Member member);
+    ResponseJoinInfo save(Member member);
     // 회원 조회
-    Optional<Member> findById(String memberId);
+    LoginForm findById(String memberId);
+    // 회원 중복 조회
+    Optional<String> findByIdDuplicate(String memberId);
     // 닉네임 조회
     Optional<String> findByNickname(String nickname);
-
     // 회원 비밀번호 재설정
     void update(String memberPw, String memberId);
 }
