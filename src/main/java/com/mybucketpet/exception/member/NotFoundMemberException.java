12c9383a935c08648d34d0a88d1e11b07a35076f
SungHwan <tjdghks130907@gmail.com>
2023-10-24 00:02:45 +0900

    [refactor] 커스텀 예외 클래스 생성
    - MemberException을 상속받은 커스텀 예외 클래스를 작성함
    - 회원 조회에 실패한 경우 사용하기 위한 커스텀 클래스

---
+++ src/main/java/com/mybucketpet/exception/member/NotFoundMemberException.java
@@ -0,0 +1,8 @@
+package com.mybucketpet.exception.member;
+
+public class NotFoundMemberException extends MemberException {
+
+    public NotFoundMemberException(String message) {
+        super(message);
+    }
+}
