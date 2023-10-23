61905118447341154f7215bc2cfef9a71ae9cc36
SungHwan <tjdghks130907@gmail.com>
2023-10-23 23:54:36 +0900

    [refactor] 정적 팩토리 메서드 패턴을 사용하도록 변경
    - 점층적 생성자 패턴과 자바빈즈 패턴을 사용하지 않고, 정적 팩토리 메서드 패턴을 사용하도록 변경함 (매개변수가 2개이므로 빌더 패턴은 적용하지 않음)
    - @Setter 삭제, 요청 DTO 클래스이므로 기본생성자는 필요해 @NoArgsContructor 추가 후, 접근제어를 private로 설정함
    - Bean Validation 기본 메시지 내용을 추가함

--- src/main/java/com/mybucketpet/controller/member/dto/LoginForm.java
+++ src/main/java/com/mybucketpet/controller/member/dto/LoginForm.java
@@ -1,21 +1,25 @@
 package com.mybucketpet.controller.member.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 @Getter
-@Setter
 @ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginForm {
-
+    @NotEmpty(message = "이메일을 입력해 주세요.")
     private String loginId;
+    @NotBlank(message = "비밀번호를 입력해 주세요.")
     private String loginPw;
 
-    public LoginForm() {}
-
-    public LoginForm(String loginId, String loginPw) {
+    private LoginForm(String loginId, String loginPw) {
         this.loginId = loginId;
         this.loginPw = loginPw;
+    }
+
+    public static LoginForm of(String loginId, String loginPw) {
+        return new LoginForm(loginId, loginPw);
     }
 }
