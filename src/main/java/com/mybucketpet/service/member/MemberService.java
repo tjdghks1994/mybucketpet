a02f27d14f670bc4a384d1b33f0f420f62b72e40
SungHwan <tjdghks130907@gmail.com>
2023-10-25 01:10:46 +0900

    [refactor] 엔티티 클래스 의존성 제거
    - 기존 서비스 코드에서 엔티티 클래스를 의존하고 있어 DTO 클래스를 의존하도록 소스 변경함
    - 회원 중복 조회 역할을 하는 메서드 추가함

--- src/main/java/com/mybucketpet/service/member/MemberService.java
+++ src/main/java/com/mybucketpet/service/member/MemberService.java
@@ -1,18 +1,19 @@
 package com.mybucketpet.service.member;
 
 import com.mybucketpet.controller.member.dto.JoinForm;
+import com.mybucketpet.controller.member.dto.LoginForm;
 import com.mybucketpet.controller.member.dto.PasswordChangeForm;
-import com.mybucketpet.domain.member.Member;
+import com.mybucketpet.controller.member.dto.ResponseJoinInfo;
 
 public interface MemberService {
     // 회원 가입
-    Member save(JoinForm member);
-    // 회원 조회, 필요한 정보만 전달
-    Member findById(String memberId);
+    ResponseJoinInfo save(JoinForm member);
+    // 회원 중복 조회
+    void findByIdDuplicate(String memberId);
     // 닉네임 조회
     String findByNickname(String nickName);
-    // 회원 로그인 가능 여부 체크
-    Member findByLoginAvailability(String memberId, String memberPw);
+    // 회원 로그인
+    LoginForm findByLoginAvailability(String memberId, String memberPw);
     // 회원 비밀번호 재설정
     void update(PasswordChangeForm passwordChangeForm);
 }
