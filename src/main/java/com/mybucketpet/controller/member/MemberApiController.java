c0c5d270276a4e5c770b9a3ff23414b437d18fd8
SungHwan <tjdghks130907@gmail.com>
2023-10-25 10:48:16 +0900

    [refactor] HTTP URI 설계 변경
    - 기존 회원 조회는 회원 중복 조회 개념으로 사용하고 있어 회원 중복 조회로 변경함
    - 회원 등록, 회원 중복 조회, 회원 로그인 API 작성함
    - 회원 로그인 API가 현재는 세션을 그대로 사용되도록 작성되어 있는데, 추후 리팩토링을 더 하면서 세션  저장소를 Redis로 사용하도록 수정할 예정

--- src/main/java/com/mybucketpet/controller/member/MemberApiController.java
+++ src/main/java/com/mybucketpet/controller/member/MemberApiController.java
@@ -1,23 +1,65 @@
 package com.mybucketpet.controller.member;
 
+import com.mybucketpet.controller.member.dto.JoinForm;
+import com.mybucketpet.controller.member.dto.LoginForm;
+import com.mybucketpet.controller.member.dto.ResponseJoinInfo;
+import com.mybucketpet.domain.member.Member;
+import com.mybucketpet.service.member.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberApiController {
 
+    private final MemberService memberService;
+
     /**
      * HTTP URI 설계 - API
-     *
-     * 회원 조회                /members                GET
-     * 회원 등록                /members                POST
-     * 회원 로그인               /members/login         POST
-     * 회원 비밀번호 재설정        /members/password      PATCH
-     * 회원 이메일 인증번호 발송    /members/auth-send     POST
-     * 회원 이메일 인증번호 확인    /members/auth-check    POST
-     * 회원 닉네임 조회          /members/nicknames     GET
+     * 회원 등록                /members                        POST
+     * 회원 중복 조회            /members/{memberId}/exists      GET
+     * 회원 로그인               /members/login                 POST
+     * 회원 비밀번호 재설정        /members/password              PATCH
+     * 회원 이메일 인증번호 발송    /members/auth-send             POST
+     * 회원 이메일 인증번호 확인    /members/auth-check            POST
+     * 회원 닉네임 조회          /members/nicknames             GET
      */
+
+    @PostMapping
+    public ResponseEntity<String> memberJoin(@Valid @RequestBody JoinForm joinForm) {
+        ResponseJoinInfo saveMember = memberService.save(joinForm);
+        URI location = URI.create("/members/login");
+
+        return ResponseEntity.created(location).body(saveMember.getMemberId());
+    }
+
+    @GetMapping("/{memberId}/exists")
+    public ResponseEntity<String> memberSearch(@PathVariable String memberId) {
+        memberService.findByIdDuplicate(memberId);
+
+        return ResponseEntity.ok(memberId + "는 사용 가능한 회원 ID 입니다.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginForm loginForm, HttpServletRequest request) {
+        String loginId = loginForm.getLoginId();
+        String loginPw = loginForm.getLoginPw();
+        LoginForm loginMember = memberService.findByLoginAvailability(loginId, loginPw);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("memberInfo", loginMember);
+
+        return ResponseEntity.ok(loginMember.getLoginId() + "님 로그인 성공");
+    }
 }
